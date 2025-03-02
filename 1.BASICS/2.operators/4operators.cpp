c713cdcac45dbd52dbf28efff4acdcb590c6776e
Shaik Khaja <khaja4756@gmail.com>
2025-03-02 23:43:05 +0530

    Add files via upload

---
+++ 1.BASICS/2.operators/4operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter a:- \n";
+    cin>>a;
+    cout<<"enter b:- \n";
+    cin>>b;
+    cout<<"sum= "<<(a+b)<<endl;
+    cout<<"diff= "<<(a-b)<<endl;
+    cout<<"prod= "<<(a*b)<<endl;
+    cout<<"divide= "<<(a/b)<<endl;
+    cout<<"remain= "<<(a%b)<<endl;
+    return 0;
+    }
---
+++ 1.BASICS/2.operators/5relationaloperts.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5, b=7;
+    cout<<(a>b)<<endl;
+    cout<<(9>0)<<endl;
+    cout<<(3==3)<<endl;
+    cout<<(5!=7)<<endl;
+    cout<<(3<=3)<<endl;
+    cout<<(5>=3)<<endl;
+
+    return 0;
+
+}
