cad2c8485ec3eb87ccb85279982b5c3a9b44f625
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-05-25 09:00:57 +0530

    Added comments to the solution

--- 5.Greedy Tecniques/indian_coins.cpp
+++ 5.Greedy Tecniques/indian_coins.cpp
@@ -6,18 +6,18 @@ int coins_used(vector<int> coins, int v){
     int n = coins.size();
     int ans{};
 
-    for(int i = n-1; i >= 0 && v > 0; i--){
-        if(v >= coins[i]){
-            ans += v/coins[i];
-            v = v%coins[i];
+    for(int i = n-1; i >= 0 && v > 0; i--){  // run the loop in opposite, so that you can select the highest denomination, also keeping in mind that the v(value) is not zero 
+        if(v >= coins[i]){ // check for the condition: the value which you want to satisfy must be greater than or equal to the coins[i] 
+            ans += v/coins[i]; // when we get that kind of denomination, we try to divide our value with the coin which we selected, and add it to the ans (which gives us the no. of coins used)
+            v = v%coins[i]; // find the remainder value and update our value
         }
     }
-    return ans;
+    return ans; // return the ans
 }
 
 int main(){
-    vector<int> coins = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
-    int v = 1099;
+    vector<int> coins = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};  // vector of denominations of the indian coins given
+    int v = 1099; // target to be satisfied using the above denominations 
 
     cout << coins_used(coins, v);
 
